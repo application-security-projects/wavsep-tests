f17568a7ddd46eb0c859cad9f837452ddd6012e3
Anton Abashkin <abashkin.anton@gmail.com>
2016-04-23 19:59:42 -0700

    surefire mvn fix

--- src/test/java/application/security/wavsep/tests/SingleWavsepTest.java
+++ src/test/java/application/security/wavsep/tests/SingleWavsepTest.java
@@ -1,21 +1,24 @@
 package application.security.wavsep.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SingleWavsepTest {
-
+	
 	String testURL = "http://wavsep.test:8080/wavsep/active/Reflected-XSS/RXSS-Detection-Evaluation-GET/index.jsp";
-
+	
 	// A simple test case which opens a URL and clicks the first link
 	@Test
 	public void singleWivetTest() {
-
+		
 		// Initialize WebDriver
 		HtmlUnitDriver driver = new HtmlUnitDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
 		
 		driver.get(testURL);
 
